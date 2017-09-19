023779ba481ff28b9f8a7ad045c56cb7f1501910
RnDMizeD <a.rndmized@gmail.com>
2017-09-19 17:11:13 +0100

    Added Largest and smallest in list.

---
+++ problems/largest_and_smallest_in_list.py
@@ -0,0 +1,7 @@
+#given a list
+list_unsorted = [4,2,65,999,23,765,29,45,12,0]
+#sort list
+list_sorted = sorted(list_unsorted)
+
+#print values [0] and [-1]
+print("First element:",list_sorted[0],"Last element:",list_sorted[-1])
